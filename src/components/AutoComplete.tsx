68832f706920084f68f6063021d2609fbb74c647
Daniel Winsvold <danielwinsvold@gmail.com>
2023-05-01 14:16:38 +0200

    debouncer autocomplete-liste

--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { useCombobox } from "downshift";
-import Input from "./basicComponents/Input";
-import styled from "styled-components";
-import { css } from "styled-components";
-import { AutoCompleteOption, useAutocompleteResponse } from "./LeggTilTing";
 import { Box } from "@chakra-ui/react";
+import { useCombobox } from "downshift";
+import { useMemo } from "react";
+import styled, { css } from "styled-components";
+import useDebounce from "../utils/useDebounce";
+import { useAutocompleteResponse } from "./LeggTilTing";
+import Input from "./basicComponents/Input";
 
 const DropdownStyle = styled.ul`
   position: absolute;
@@ -30,33 +30,27 @@ interface Props {
 }
 
 function Autocomplete(props: Props) {
-  const [items, setItems] = useState<AutoCompleteOption[]>([]);
-
   const autoCompleteData = useAutocompleteResponse().data;
 
-  const soretdAutocompleteData = useMemo(
-    () => autoCompleteData?.options.sort((a, b) => b.timesUsed - a.timesUsed) || [],
-    [autoCompleteData]
-  );
+  const debouncedValue = useDebounce(props.value, 500);
 
-  // Autocomplete items hentes inn asynkront, må derfor håndtere at de kan komme inn etter at komponenten har mounta
-  useEffect(() => {
-    return setItems(soretdAutocompleteData);
-  }, [soretdAutocompleteData]);
+  const autoCompleteOptions = useMemo(() => {
+    const sortedOptions = autoCompleteData?.options.sort((a, b) => b.timesUsed - a.timesUsed) || [];
+
+    if (!debouncedValue) return sortedOptions;
+
+    const possibleMatches = sortedOptions.filter((item) =>
+      item.name.toLowerCase().startsWith(debouncedValue.toLowerCase())
+    );
+
+    return possibleMatches;
+  }, [autoCompleteData, debouncedValue]);
 
   const combobox = useCombobox({
-    items: items,
+    items: autoCompleteOptions,
     inputValue: props.value,
     onInputValueChange: ({ inputValue }) => {
       props.onChange(inputValue || "");
-      if (inputValue) {
-        const possibleMatches = soretdAutocompleteData.filter((item) =>
-          item.name.toLowerCase().startsWith(inputValue.toLowerCase())
-        );
-        setItems(possibleMatches);
-      } else {
-        setItems(soretdAutocompleteData);
-      }
     },
     itemToString: (item) => item?.name ?? "N/A",
     onSelectedItemChange: (change) => change.selectedItem && props.onChange(change.selectedItem.name),
@@ -69,7 +63,7 @@ function Autocomplete(props: Props) {
       </div>
       <DropdownStyle {...combobox.getMenuProps()}>
         {combobox.isOpen &&
-          items.slice(0, 10).map((item, index) => (
+          autoCompleteOptions.slice(0, 10).map((item, index) => (
             <ItemStyle
               {...combobox.getItemProps({ item, index })}
               highLighted={combobox.highlightedIndex === index}
